e212b2e7bdc7efacec285440ef65871dc5825798
eeerika <egg@ucsb.edu>
2023-04-22 21:42:56 -0700

    switch to water year column being used to streamline code; add some more comments

--- calc_almond_yield_anomaly.R
+++ calc_almond_yield_anomaly.R
@@ -15,22 +15,21 @@ library(here)
 library(tidyverse)
 
 calc_almond_yield_anomaly <- function(clim_data = "clim.txt", a = -0.015, b = -0.0046, c = -0.07, d = 0.0043, e = 0.28) {
-  clim_df <- read.table(here(paste0("data/", clim_data)), header = TRUE) # clim_data
+  clim_df <- read.table(here(paste0("data/", clim_data)), header = TRUE) # read in the climate data
   
   clim_df_prepped <- clim_df %>%
-    group_by(month, year) %>%
-    summarize(max_tmax_c = max(tmax_c),
-              min_tmin_c = min(tmin_c),
-              sum_precip = sum(precip))
+    group_by(month, wy) %>% # group by month and water year
+    summarize(max_tmax_c = max(tmax_c), # get max of max temps for the month of year (not used for almonds)
+              min_tmin_c = min(tmin_c), # get min of min temps for the month of year
+              sum_precip = sum(precip)) # get sum of precip for the month of year
   
-  years <- sort(unlist(unique(clim_df_prepped$year)))
-  years <- years[2:23] # we don't want 1988
+  years <- sort(unlist(unique(clim_df_prepped$wy))) # get an ordered list of years for the loop
   
-  yield_list <- c()
+  yield_list <- c() # initialize list to hold yield anomalies by year
   
   for (i in seq_along(years)) {
     clim_df_prepped_filtered <- clim_df_prepped %>%
-      filter(year == years[[i]]) # filter df to year at hand
+      filter(wy == years[[i]]) # filter df to year at hand
     
     min_T_2 <- clim_df_prepped_filtered$min_tmin_c[[2]] # get min_T_2 (min temp from month 2)
 
@@ -43,5 +42,5 @@ calc_almond_yield_anomaly <- function(clim_data = "clim.txt", a = -0.015, b = -0
   
   return(c(min_yield_anomaly = min(yield_list),
            max_yield_anomaly = max(yield_list),
-           mean_yield_anomaly = mean(yield_list))) # output the yield anomaly min, max, mean
+           mean_yield_anomaly = mean(yield_list))) # output the yield anomaly min, max, mean across all years
 }
