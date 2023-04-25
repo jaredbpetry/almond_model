ecee2aeeb2e7b81864df844a488eab2dafe0e8cd
eeerika <egg@ucsb.edu>
2023-04-25 10:40:04 -0700

    add defaults and new param names to R, Rmd, pdf

--- calc_almond_yield_anomaly.R
+++ calc_almond_yield_anomaly.R
@@ -3,11 +3,11 @@
 #' A function that computes almond yield anomalies (ton/acre) for years in a dataframe, converting daily information on minimum temperatures and precipitation to monthly information and using these in an equation with set numerical parameters by default.
 #'
 #' @param clim_data A character string ending in .txt referring to climate data with variables for daily minimum temperature in degrees Celsius (named tmin_c) and daily precipitation in mm (named precip), with corresponding month and year columns (named month and wy). This should be stored in the working directory in a folder called data. By default, this will search for "clim.txt."
-#' @param a A number corresponding to the almond yield anomaly parameter that is multiplied by the minimum minimum temperature of the second month of the year.
-#' @param b A number corresponding to the almond yield anomaly parameter that is multiplied by the minimum minimum temperature of the second month of the year, squared.
-#' @param c A number corresponding to the almond yield anomaly parameter that is multiplied by the sum of the precipitation of the first month of the year.
-#' @param d A number corresponding to the almond yield anomaly parameter that is multiplied by the sum of the precipitation of the first month of the year, squared.
-#' @param e A number that is added to all of the other parts of the almond yield anomaly equation.
+#' @param temp_param A number corresponding to the almond yield anomaly parameter that is multiplied by the minimum minimum temperature of the second month of the year. Default = -0.015
+#' @param sq_temp_param A number corresponding to the almond yield anomaly parameter that is multiplied by the minimum minimum temperature of the second month of the year, squared. Default = -0.0046
+#' @param precip_param A number corresponding to the almond yield anomaly parameter that is multiplied by the sum of the precipitation of the first month of the year. Default = -0.07
+#' @param sq_precip_param A number corresponding to the almond yield anomaly parameter that is multiplied by the sum of the precipitation of the first month of the year, squared. Default = 0.0043
+#' @param constant A number that is added to all of the other parts of the almond yield anomaly equation. Default = 0.28
 #'
 #' @return The minimum, maximum, and mean almond yield anomalies (ton/acre) across all years of the dataframe
 #' @export
@@ -20,7 +20,7 @@
 library(here)
 library(tidyverse)
 
-calc_almond_yield_anomaly <- function(clim_data = "clim.txt", a = -0.015, b = -0.0046, c = -0.07, d = 0.0043, e = 0.28) {
+calc_almond_yield_anomaly <- function(clim_data = "clim.txt", temp_param = -0.015, sq_temp_param = -0.0046, precip_param = -0.07, sq_precip_param = 0.0043, constant = 0.28) {
   clim_df <- read.table(here(paste0("data/", clim_data)), header = TRUE) # read in the climate data
   
   clim_df_prepped <- clim_df %>%
@@ -40,7 +40,7 @@ calc_almond_yield_anomaly <- function(clim_data = "clim.txt", a = -0.015, b = -0
 
     precip_1 <- clim_df_prepped_filtered$sum_precip[[1]] # get precip_1 (precip from month 1)
     
-    Y <- (a * min_T_2) + (b * (min_T_2^2)) + (c * precip_1) + (d * (precip_1^2)) + e # calculate yield anomaly with formula
+    Y <- (temp_param * min_T_2) + (sq_temp_param * (min_T_2^2)) + (precip_param * precip_1) + (sq_precip_param * (precip_1^2)) + constant # calculate yield anomaly with formula
     
     yield_list <- append(yield_list, Y) # add resulting value to list, and continue to next year if one exists
   }
